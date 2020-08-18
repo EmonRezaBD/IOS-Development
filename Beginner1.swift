4ec597eb189a6bc5e59353966d4ab73ffadec116
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-08-18 21:32:08 +0600

    Create Beginner1.swift

---
+++ Beginner1.swift
@@ -0,0 +1,33 @@
+//I use playground for coding and Xcode 8
+
+import UIKit
+
+var x = "Bismillah"
+
+var flag:Bool = false
+print(flag)
+
+var y:Float = 34.9999980
+print(Float(y))
+
+//x = String (56)
+
+if x=="Bismillah"{
+    print("0")
+}
+else {
+    print("NO")
+}
+var option:Character = "a"
+switch option {
+case "a","b","c":
+    print("A or B")
+case "d":
+    print("This is d.")
+default:
+    print("Nope.")
+}
+//For in Loop
+
+
+
